391fd5c7e90d671c00ecdc41df3a86a148f487e6
ZarekTorres <76668750+ZarekTorres@users.noreply.github.com>
2022-08-12 19:05:18 -0500

    Added main code to run

---
+++ main.py
@@ -0,0 +1,13 @@
+from AreaCalculator import *
+
+
+def main():
+
+    window = Tk()
+    widgets = GUI(window)
+    window.title("Area of Shapes")
+    window.mainloop()
+
+
+if __name__ == '__main__':
+    main()
